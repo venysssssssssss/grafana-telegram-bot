ee974af746bac7c438de9c594e7aa10f6e727055
venysssssssssss <shablaur7@hotmail.com>
2024-07-29 21:36:47 -0300

    Refactor data processing code to use DataProcessor class

--- app/main.py
+++ app/main.py
@@ -1,69 +1,34 @@
 import os
 import time
-
-from authentication import authenticate
-from browser import (clean_download_directory, rename_latest_file,
-                     scroll_page_to_table, start_browser)
-from data_processing import analyze_data
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
+from browser import BrowserManager
+from authentication import Authenticator
+from data_processing import DataProcessor
+from action_manager import ActionManager
 
 
 def main():
-    download_path = clean_download_directory('data')
-    driver = start_browser(download_path)
-    authenticate(driver, 'guilherme.caseiro@tahto.com.br', 'C453iro@102030.')
-    scroll_page_to_table(driver)
-    time.sleep(3)
-    target_element = WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located((By.XPATH, '//*[@id=":rl:"]'))
-    )
-    action = ActionChains(driver)
-    action.move_to_element(target_element).perform()
-    time.sleep(2)
-    action.send_keys('i').perform()
-    dropdown_element = WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located(
-            (
-                By.XPATH,
-                '//*[@id="reactRoot"]/div[1]/div/div[3]/div[3]/div/div/div[2]/div[1]/div/div/div[1]/div/div/div/div[1]/button',
-            )
-        )
-    )
-    time.sleep(4)
-    action.click(dropdown_element).perform()
-    toggle_xlsx = WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located(
-            (
-                By.XPATH,
-                '//*[@id="reactRoot"]/div[1]/div/div[3]/div[3]/div/div/div[2]/div[1]/div/div/div[1]/div/div/div[2]/div/div/div/div/div[3]/div/div[2]/div/div/label',
-            )
-        )
-    )
-    time.sleep(0.5)
-    action.click(toggle_xlsx).perform()
-    download_button = WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located(
-            (
-                By.XPATH,
-                '//*[@id="reactRoot"]/div[1]/div/div[3]/div[3]/div/div/div[2]/div[1]/div/div/div[1]/div/div/div[1]/div[2]/button',
-            )
-        )
-    )
-    time.sleep(0.5)
-    action.click(download_button).perform()
-    time.sleep(
-        8
-    )  # Ajuste o tempo conforme necessário para garantir que o download seja concluído
-    driver.quit()
+    browser_manager = BrowserManager('data')
+    download_path = browser_manager.clean_download_directory('data')
 
-    # Path to the downloaded CSV file
-    rename_latest_file(download_path, 'relatorio.csv')
-    metrics = analyze_data(os.path.join(download_path, 'relatorio.csv'))
+    auth = Authenticator(browser_manager.driver)
+    auth.authenticate()
+    browser_manager.scroll_to_table()
+    actions = ActionManager(browser_manager.driver)
+
+    actions.move_to_and_interact('//*[@id=":rl:"]', 'i')
+    actions.click_element('//*[@id="reactRoot"]/div[1]/div/div[3]/div[3]/div/div/div[2]/div[1]/div/div/div[1]/div/div/div/div[1]/button')
+    actions.click_element('//*[@id="reactRoot"]/div[1]/div/div[3]/div[3]/div/div/div[2]/div[1]/div/div/div[1]/div/div/div[2]/div/div/div/div/div[3]/div/div[2]/div/div/label')
+    actions.click_element('//*[@id="reactRoot"]/div[1]/div/div[3]/div[3]/div/div/div[2]/div[1]/div/div/div[1]/div/div/div[1]/div[2]/button')
+
+    # Aguarda o download ser concluído
+    browser_manager.rename_latest_file(download_path, 'relatorio.csv')
+
+    data_processor = DataProcessor(os.path.join(download_path, 'relatorio.csv'))
+    metrics = data_processor.analyze_data()
     print('Returned metrics:', metrics)
 
+    browser_manager.driver.quit()
 
 if __name__ == '__main__':
     main()
+
