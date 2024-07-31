cd2715643bb3ea41377272fd4400499aa713ee09
venysssssssssss <shablaur7@hotmail.com>
2024-07-31 12:31:30 -0300

    Refactor schedule_regular_collections to use a dictionary for scheduling regular collections

--- app/schedule_regular.py
+++ app/schedule_regular.py
@@ -1,29 +1,18 @@
 import schedule
 from send_telegram_msg import send_informational_message
 
-def schedule_for_day(day, times, driver):
+def schedule_for_day(day, times, func, *args):
     for time in times:
-        getattr(schedule.every(), day).at(time).do(send_informational_message, driver)
+        getattr(schedule.every(), day).at(time).do(func, *args)
 
-def schedule_regular_collections(driver):
-    """
-    Schedule regular collections based on a dictionary of weekdays and corresponding times.
-
-    Args:
-        driver: The driver object used for scheduling.
-
-    Returns:
-        None
-    """
-
+def schedule_regular_collections(driver, tme_xpath, tef_xpath, backlog_xpath):
     schedule_dict = {
         'monday': ['08:05', '12:05', '16:05', '20:05'],
         'tuesday': ['08:05', '12:05', '16:05', '20:05'],
-        'wednesday': ['08:05', '14:38', '16:05', '20:05', '10:45'],
+        'wednesday': ['08:05', '10:45', '12:30', '14:38', '16:05', '20:05'],
         'thursday': ['08:05', '12:05', '16:05', '20:05'],
         'friday': ['08:05', '12:05', '16:05', '20:05'],
-        'saturday': ['09:05', '12:05', '15:55']
+        'saturday': ['09:05', '12:05', '15:55'],
     }
-
     for day, times in schedule_dict.items():
-        schedule_for_day(day, times, driver)
+        schedule_for_day(day, times, send_informational_message, driver, tme_xpath, tef_xpath, backlog_xpath)
