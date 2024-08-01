4adb2fbef225904eba1653ebef66f445213cbe04
venysssssssssss <shablaur7@hotmail.com>
2024-08-01 13:43:13 -0300

    Refactor schedule_regular_collections to update Thursday schedule time

--- app/schedule_regular.py
+++ app/schedule_regular.py
@@ -1,8 +1,14 @@
 import schedule
+import logging
 from send_telegram_msg import send_informational_message
+
+# Configurar logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+logger = logging.getLogger()
 
 def schedule_for_day(day, times, func, *args):
     for time in times:
+        logger.info(f"Agendando tarefa para {day} às {time}")
         getattr(schedule.every(), day).at(time).do(func, *args)
 
 def schedule_regular_collections(driver, tme_xpath, tef_xpath, backlog_xpath):
@@ -10,9 +16,10 @@ def schedule_regular_collections(driver, tme_xpath, tef_xpath, backlog_xpath):
         'monday': ['08:05', '12:05', '16:05', '20:05'],
         'tuesday': ['08:05', '12:05', '16:05', '20:05'],
         'wednesday': ['08:05', '10:45', '13:01', '14:38', '16:05', '20:05'],
-        'thursday': ['08:05', '12:05', '16:05', '20:05', '12:48', '13:20'],
+        'thursday': ['08:05', '12:05', '16:05', '20:05', '12:48', '13:46'],
         'friday': ['08:05', '12:05', '16:05', '20:05'],
         'saturday': ['09:05', '12:05', '15:55'],
     }
     for day, times in schedule_dict.items():
         schedule_for_day(day, times, send_informational_message, driver, tme_xpath, tef_xpath, backlog_xpath)
+    logger.info("Tarefas de agendamento configuradas")
