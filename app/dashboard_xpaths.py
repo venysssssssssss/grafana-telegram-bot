a5bb2edc13852902d61e27c85ced8bdb4b5fdf1a
venysssssssssss <shablaur7@hotmail.com>
2024-09-24 21:11:52 -0300

    Refactor dashboard_xpaths.py to add MVP-specific xpaths

---
+++ app/dashboard_xpaths.py
@@ -0,0 +1,23 @@
+DASHBOARD_XPATHS = {
+    "mvp1": {
+        "var_exibir": '//*[@id="var-exibir"]',
+        "opcao_exibir": '//*[@id="options-exibir"]/li[4]/button',
+        "tme": '/html/body/div[1]/div[1]/div/main/div/div/div[3]/div/div[1]/div/div/div[1]/div/div/div[5]/div/div/div[3]/div/div/div/div/div/div/span',
+        "tef": '//*[@id=":rj:"]/div/div/div/div/div/div/span',
+        "backlog": '//*[@id=":re:"]/div/div/div/div/div/div/span',
+    },
+    "mvp2": {
+        "var_exibir": '//*[@id="var-exibir"]',
+        "opcao_exibir": '//*[@id="options-exibir"]/li[2]/button',
+        "tme": '/html/body/div[1]/div[2]/div/main/div/div/div[3]/div/div[1]/div/div/div[2]/div/div/div[5]/div/div/div[3]/div/div/div/div/div/div/span',
+        "tef": '//*[@id=":xy:"]/div/div/div/div/div/div/span',
+        "backlog": '//*[@id=":yz:"]/div/div/div/div/div/div/span',
+    },
+    "mvp3": {
+        "var_exibir": '//*[@id="var-exibir"]',
+        "opcao_exibir": '//*[@id="options-exibir"]/li[3]/button',
+        "tme": '/html/body/div[1]/div[1]/div/main/div/div/div[3]/div/div[1]/div/div/div[1]/div/div/div[5]/div/div/div[3]/div/div/div/div[1]/div/div/span',
+        "tef": '//*[@id=":rj:"]/div/div/div/div/div/div/span',
+        "backlog": '/html/body/div[1]/div[1]/div/main/div/div/div[3]/div/div[1]/div/div/div[1]/div/div/div[1]/div/div/div[3]/div/div/div/div/div/div/span',
+    }
+}
