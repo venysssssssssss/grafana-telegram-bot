02c9d2b23c301a841b51a0ac7938afabc64f02a8
venysssssssssss <shablaur7@hotmail.com>
2024-10-21 17:07:35 -0300

    Refactor main.py to delay file removal after sending message

--- app/main.py
+++ app/main.py
@@ -155,7 +155,14 @@ def process_dashboard(
             if initial_run and relatorio_path:
                 logger.info('Relatorio path: ', relatorio_path)
 
-                time.sleep(1)
+                send_informational_message(
+                    driver,
+                    result['tme'],
+                    result['tef'],
+                    result['backlog'],
+                    relatorio_path,  # Caminho correto para cada dashboard
+                    dashboard_name,
+                )
 
                 # Excluir o arquivo após enviar a mensagem
                 logger.info(f'Removendo o arquivo {relatorio_path}')
