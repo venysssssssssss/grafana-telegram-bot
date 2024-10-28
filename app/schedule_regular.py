21e53058e11fdd844513d2796da61235a929361a
venysssssssssss <shablaur7@hotmail.com>
2024-10-28 12:57:14 -0300

    Refactor schedule_regular.py to improve dashboard data collection and messaging

--- app/schedule_regular.py
+++ app/schedule_regular.py
@@ -1,70 +1,61 @@
 import logging
 import os
 import time
-import urllib3
+
 import schedule
-from action_manager import ActionManager
 from authentication import Authenticator
 from browser import BrowserManager
-from collect_data import collect_data_from_dashboard
 from execute_download_actions import execute_download_actions
 from monitor_falhas import iniciar_monitoramento, pausar_monitoramento
-from selenium.common.exceptions import (NoSuchElementException,
-                                        WebDriverException)
-from selenium.webdriver.remote.remote_connection import RemoteConnection
+from selenium.common.exceptions import WebDriverException
 from send_telegram_msg import send_informational_message
-
-urllib3.util.retry.Retry.DEFAULT_BACKOFF_MAX = 1
 
 # Configurar logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 logger = logging.getLogger()
 
+reiniciar_navegador = BrowserManager.reiniciar_navegador
+
+
 def download_and_send_message_for_dashboard(
-    dashboard_name,
-    driver,
-    actions,
-    browser_manager,
-    kpi_data,
-    download_path,
-    initial_run=True,
+    dashboard_name, driver, actions, browser_manager, kpi_data, download_path
 ):
     try:
+        # Pausar monitoramento de falhas
         pausar_monitoramento()
-        logger.info(f'Iniciando processo para o dashboard {dashboard_name}')
 
-        logger.info(f'Acessando {dashboard_name}')
-        if initial_run:
-            time.sleep(5)  # Aguardar carregamento do dashboard
+        browser_manager = BrowserManager(os.path.join(os.getcwd(), 'data'))
+        driver = browser_manager.driver
 
-            #if dashboard_name.lower() == 'mvp1':
-             #   auth = Authenticator(driver)
-              #  auth.authenticate()
-               # logger.info(f'Autenticação concluída para {dashboard_name}')
+        time.sleep(3)
+
+        # Acessar o dashboard específico
+        if dashboard_name == 'MVP1':
+            # Executa autenticação no MVP1
+            logger.info(f'Autenticando no {dashboard_name}')
+            driver.get(
+                'https://e-bots.co/grafana/d/b12d0f69-2249-46c9-9a3d-da56588d47f4/ebots-detalhe-do-robo?orgId=1&refresh=5m&var-Robot=tahto-pap&var-Robot_id=51&var-exibir_itens=processados&var-exibir_10000&var-exibir_tarefas=todas'
+            )
+            auth = Authenticator(driver)
+            auth.authenticate()
+            logger.info(f'Autenticação concluída para {dashboard_name}')
         else:
-            driver.refresh()
-            time.sleep(5)  # Esperar recarregamento da página
+            # Acessar o MVP3
+            driver.get(
+                'https://e-bots.co/grafana/d/b12d0f69-2249-46c9-9a3d-da56588d47f4/ebots-detalhe-do-robo?var-Robot=tahto-pap-mvp3&orgId=1&refresh=5m&var-Robot_id=92&var-exibir_itens=processados&var-exibir=10000&var-exibir_tarefas=todas'
+            )
 
-        # Coletar e processar dados
-        result = collect_data_from_dashboard(
-            driver,
-            dashboard_name,
-            actions,
-            browser_manager,
-            download_path,
-            initial_run,
+        # Realiza o scroll e o download dos dados para o dashboard específico
+        logger.info(f'Iniciando processo de download para {dashboard_name}')
+        time.sleep(1)
+        relatorio_path = execute_download_actions(
+            actions, browser_manager, download_path, dashboard_name
         )
-
-        if not result or not result.get('relatorio_path'):
-            logger.error(f'Falha ao coletar dados do {dashboard_name}')
-            return
-
-        relatorio_path = result['relatorio_path']
         logger.info(
-            f'Relatório baixado para {dashboard_name}: {relatorio_path}'
+            f'Relatório baixado para {dashboard_name} no caminho: {relatorio_path}'
         )
 
-        # Enviar mensagem com KPIs e caminho do relatório
+        # Envia a mensagem com as KPIs coletadas e o caminho do relatório
         send_informational_message(
             driver,
             kpi_data['tme'],
@@ -75,102 +66,82 @@ def download_and_send_message_for_dashboard(
         )
         logger.info(f'Mensagem enviada para o dashboard {dashboard_name}')
 
-        # Excluir o arquivo após o envio da mensagem
-        if os.path.exists(relatorio_path):
-            os.remove(relatorio_path)
-            logger.info(f'Arquivo {relatorio_path} removido com sucesso.')
-
-    except (WebDriverException, NoSuchElementException) as e:
-        logger.error(
-            f'Erro no WebDriver ou elemento não encontrado para {dashboard_name}: {e}'
-        )
-    except Exception as e:
-        logger.error(f'Erro inesperado ao processar {dashboard_name}: {e}')
-    finally:
+        # Reiniciar monitoramento de falhas
+        time.sleep(15)
         iniciar_monitoramento()
-        logger.info(f'Monitoramento reiniciado para {dashboard_name}')
 
-
-def execute_dashboard_task(
-    driver, actions, browser_manager, kpis, dashboard_name, url
-):
-    try:
-        # Verificar e reiniciar o navegador se necessário
-        if not browser_manager.driver or not browser_manager.driver.session_id:
-            logger.info('Iniciando uma nova sessão do navegador.')
-            browser_manager.reiniciar_navegador()
-
-        driver = browser_manager.driver  # Reutilizar a instância correta
-
-        # Limpar o diretório de downloads
-        download_path = browser_manager.clean_download_directory('data')
-
-        # Acessar o dashboard
-        logger.info(f'Acessando {dashboard_name} em {url}')
-        driver.get(url)
-
-        # Aguardar para garantir que o dashboard foi completamente carregado
-        time.sleep(5)
-
-        # Executar as ações de download e envio da mensagem
-        download_and_send_message_for_dashboard(
-            dashboard_name,
-            driver,
-            actions,
-            browser_manager,
-            kpis,
-            download_path,
-        )
     except WebDriverException as e:
-        logger.error(f'Erro ao acessar {dashboard_name}: {e}')
+        logger.error(
+            f'Erro no WebDriver durante a coleta de dados no {dashboard_name}: {e}'
+        )
+        raise e
 
 
 def download_and_send_message_for_mvp1(
     driver, actions_mvp1, browser_manager, kpis_mvp1
 ):
-    url = (
-        'https://e-bots.co/grafana/d/b12d0f69-2249-46c9-9a3d-da56588d47f4/'
-        'ebots-detalhe-do-robo?orgId=1&refresh=5m&var-Robot=tahto-pap&'
-        'var-Robot_id=51&var-exibir_itens=processados&var-exibir_10000&var-exibir_tarefas=todas'
-    )
-    execute_dashboard_task(
-        driver, actions_mvp1, browser_manager, kpis_mvp1, 'MVP1', url
-    )
+    try:
+        # Limpa o diretório de downloads
+        download_path = browser_manager.clean_download_directory('data')
+
+        # Executa o processo de download e envio de mensagem para o MVP1
+        download_and_send_message_for_dashboard(
+            'MVP1',
+            driver,
+            actions_mvp1,
+            browser_manager,
+            kpis_mvp1,
+            download_path,
+        )
+
+    except WebDriverException as e:
+        logger.error(f'Erro no WebDriver ao tentar acessar MVP1: {e}')
 
 
 def download_and_send_message_for_mvp3(
     driver, actions_mvp3, browser_manager, kpis_mvp3
 ):
-    url = (
-        'https://e-bots.co/grafana/d/b12d0f69-2249-46c9-9a3d-da56588d47f4/'
-        'ebots-detalhe-do-robo?var-Robot=tahto-pap-mvp3&orgId=1&refresh=5m&'
-        'var-Robot_id=92&var-exibir_itens=processados&var-exibir=10000&var-exibir_tarefas=todas&'
-        'from=now%2Fd&to=now%2Fd'
-    )
-    execute_dashboard_task(
-        driver, actions_mvp3, browser_manager, kpis_mvp3, 'MVP3', url
-    )
+    try:
+        # Limpa o diretório de downloads
+        download_path = browser_manager.clean_download_directory('data')
+
+        # Executa o processo de download e envio de mensagem para o MVP3
+        download_and_send_message_for_dashboard(
+            'MVP3',
+            driver,
+            actions_mvp3,
+            browser_manager,
+            kpis_mvp3,
+            download_path,
+        )
+
+    except WebDriverException as e:
+        logger.error(f'Erro no WebDriver ao tentar acessar MVP3: {e}')
 
 
 def schedule_for_day(day, times, func, *args):
     for time in times:
-        logger.info(f'Agendando {func.__name__} para {day} às {time}')
+        logger.info(f'Agendando tarefa para {day} às {time}')
         getattr(schedule.every(), day).at(time).do(func, *args)
 
 
 def schedule_regular_collections(
     driver, actions_mvp1, actions_mvp3, browser_manager, kpis_mvp1, kpis_mvp3
 ):
+    """
+    Configura o agendamento regular para os dashboards MVP1 e MVP3.
+    """
     schedule_dict = {
         'monday': ['08:05', '12:05', '16:15', '20:05'],
         'tuesday': ['08:05', '12:05', '16:15', '20:05'],
         'wednesday': ['08:05', '12:05', '16:15', '20:05'],
         'thursday': ['08:05', '12:05', '16:15', '20:05'],
-        'friday': ['08:05', '14:06', '16:15', '20:05'],
+        'friday': ['08:05', '12:05', '17:46', '20:05'],
         'saturday': ['09:05', '12:05', '15:55'],
     }
 
     for day, times in schedule_dict.items():
+        # Agendar MVP1
         schedule_for_day(
             day,
             times,
@@ -180,6 +151,8 @@ def schedule_regular_collections(
             browser_manager,
             kpis_mvp1,
         )
+
+        # Agendar MVP3
         schedule_for_day(
             day,
             times,
