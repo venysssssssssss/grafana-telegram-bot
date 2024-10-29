0b4f2cd178c66d11b4a9fae2054eb2581c1b9ad6
venysssssssssss <shablaur7@hotmail.com>
2024-10-29 16:44:35 -0300

    Refactor collect_data.py to improve logging and error handling

--- app/collect_data.py
+++ app/collect_data.py
@@ -19,7 +19,11 @@ def collect_data_from_dashboard(
     download_path,
     initial_run=True,
 ):
+    logger.info(f'Iniciando coleta de dados para o dashboard: {dashboard_name}')
+    
     xpaths = DASHBOARD_XPATHS.get(dashboard_name)
+
+    logger.info(f'XPaths encontrados: {xpaths}')
 
     if not xpaths:
         logger.error(f'Dashboard {dashboard_name} não encontrado!')
@@ -27,41 +31,42 @@ def collect_data_from_dashboard(
 
     try:
         if initial_run:
-            # Interagir com os elementos específicos do dashboard apenas na primeira vez
+            logger.info(f'Primeira execução: acessando elementos do dashboard {dashboard_name}')
             actions.click_element(xpaths['var_exibir'])
             actions.click_element(xpaths['opcao_exibir'])
+        
+        # Verifica se o driver ainda está disponível
+        if not driver:
+            logger.error("WebDriver não está disponível! Reiniciando o navegador.")
+            browser_manager.reiniciar_navegador()
+            return None
 
-        # Scroll até a tabela - Executado sempre antes de coletar dados ou fazer download
+        # Scroll até a tabela
+        logger.info(f'Realizando scroll no dashboard {dashboard_name}')
         browser_manager.scroll_to_table()
-        logger.info(f'Scroll até a tabela concluído para {dashboard_name}')
-
+        
+        # Iniciar download se for a primeira execução
         if initial_run:
-            # Executar ações de download com o nome do dashboard
+            logger.info(f'Iniciando download no dashboard {dashboard_name}')
             relatorio_path = execute_download_actions(
                 actions, browser_manager, download_path, dashboard_name
             )
-            logger.info(
-                f'Relatório baixado para o {dashboard_name} no caminho: {relatorio_path}'
-            )
+            if relatorio_path:
+                logger.info(f'Relatório baixado com sucesso no caminho: {relatorio_path}')
+            else:
+                logger.warning(f'Falha no download do relatório para o dashboard {dashboard_name}')
         else:
             relatorio_path = None
 
-        # Coletar TME, TEF, e Backlog
+        # Coletar KPIs
+        logger.info(f'Coletando KPIs para o dashboard {dashboard_name}')
         tme_element = actions.find_element(xpaths['tme'])
-        tme_xpath = (
-            tme_element.text if tme_element.text != 'No data' else '00:00:00'
-        )
-
+        tme_xpath = tme_element.text if tme_element.text != 'No data' else '00:00:00'
         tef_element = actions.find_element(xpaths['tef'])
-        tef_xpath = (
-            tef_element.text if tef_element.text != 'No data' else '00:00:00'
-        )
-
+        tef_xpath = tef_element.text if tef_element.text != 'No data' else '00:00:00'
         backlog_xpath = actions.find_element(xpaths['backlog']).text
-
-        logger.info(
-            f'KPIs coletados para {dashboard_name}: TME={tme_xpath}, TEF={tef_xpath}, Backlog={backlog_xpath}'
-        )
+        
+        logger.info(f'KPIs coletados: TME={tme_xpath}, TEF={tef_xpath}, Backlog={backlog_xpath}')
 
         return {
             'tme': tme_xpath,
@@ -69,18 +74,14 @@ def collect_data_from_dashboard(
             'backlog': backlog_xpath,
             'relatorio_path': relatorio_path,
         }
+
     except NoSuchElementException as e:
-        logger.error(
-            f'Elemento não encontrado durante a coleta de dados no {dashboard_name}: {e}'
-        )
+        logger.error(f'Elemento não encontrado: {e}', exc_info=True)
         return None
     except WebDriverException as e:
-        logger.error(
-            f'Erro no WebDriver durante a coleta de dados no {dashboard_name}: {e}'
-        )
+        logger.error(f'Erro no WebDriver: {e}', exc_info=True)
         return None
     except Exception as e:
-        logger.error(
-            f'Erro inesperado ao coletar dados do {dashboard_name}: {e}'
-        )
+        logger.error(f'Erro inesperado: {e}', exc_info=True)
         return None
+
